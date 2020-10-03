309afbf97909b8475fabba6cef66c5b84e503823
Grzegorz Skrzypczak <pplocharski@MacBook-Air-Cobra.local>
2020-10-03 11:48:50 +0200

    init(paa): init project

---
+++ src/test/java/pl/sdaacademy/pokemonacademyapi/PokemonAcademyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdaacademy.pokemonacademyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonAcademyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
