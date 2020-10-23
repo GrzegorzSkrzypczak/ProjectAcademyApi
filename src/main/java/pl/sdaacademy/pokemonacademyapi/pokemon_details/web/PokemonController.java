2c55e73ceca2e33902650e579eeb1602cc2505f8
Grzegorz <rowingteam.grzegorz@gmail.com>
2020-10-23 14:14:20 +0200

    feature(paa): fixed getPokemonDetails by name in PokemonDetailService

--- src/main/java/pl/sdaacademy/pokemonacademyapi/pokemon_details/web/PokemonController.java
+++ src/main/java/pl/sdaacademy/pokemonacademyapi/pokemon_details/web/PokemonController.java
@@ -24,10 +24,10 @@ public class PokemonController {
         this.pokemonListService = pokemonListService;
     }
 
-//    @GetMapping("/{name}")
-//    public PokemonDetails getPokemon(@PathVariable String name) {
-//        return pokemonDetailService.getPokemonDetails(name);
-//    }
+    @GetMapping("/{name}")
+    public PokemonDetails getPokemon(@PathVariable String name) {
+        return pokemonDetailService.getPokemonDetails(name);
+    }
 
     @GetMapping
     public List<PokemonDetails> getPokemons(@RequestParam List<String> pokemonNames) {
